94a46d7fe2c11b6874099f022be9ac5260706f9c
deadcam2020 <85250604+deadcam2020@users.noreply.github.com>
2021-10-09 15:24:12 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,38 +1,9 @@
 <?php 
 $destinatario = "camilocontreras2019@gmail.com"; 
 $asunto = "Este mensaje es de prueba"; 
-$cuerpo = ' 
-<html> 
-<head> 
-   <title>Prueba de correo</title> 
-</head> 
-<body> 
-<h1>Hola amigos!</h1> 
-<p> 
-<b>Bienvenidos a mi correo electrónico de prueba</b>. Estoy encantado de tener tantos lectores. Este cuerpo del mensaje es del artículo de envío de mails por PHP. Habría que cambiarlo para poner tu propio cuerpo. Por cierto, cambia también las cabeceras del mensaje. 
-</p> 
-</body> 
-</html> 
-'; 
-//para el envío en formato HTML 
-$headers = "MIME-Version: 1.0\r\n"; 
-$headers .= "Content-type: text/html; charset=iso-8859-1\r\n"; 
-
-//dirección del remitente 
-$headers .= "From: Camilo Contreras <camilocontreras2019@gmail.com>\r\n"; 
-
-//dirección de respuesta, si queremos que sea distinta que la del remitente 
-$headers .= "Reply-To: camilo-contreras@corposucre.edu.co\r\n"; 
-
-//ruta del mensaje desde origen a destino 
-$headers .= "Return-path: camilocontreras2019@gmail.com\r\n"; 
-
-//direcciones que recibián copia 
-$headers .= "Cc: camilo-contreras@corposucre.edu.co\r\n"; 
-
-//direcciones que recibirán copia oculta 
-$headers .= "Bcc: camilocontreras2019@gmail.com\r\n"; 
+$mensaje = "Hola, esta es una prueba de cómo envíar correos electrónicos con PHP";
 
 
-mail($destinatario,$asunto,$cuerpo, $headers) 
+
+mail($destinatario,$asunto,$mensaje) 
 ?>
