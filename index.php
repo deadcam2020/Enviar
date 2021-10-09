39df82cfebb2ce1a53bc3dfa78824f45d7686f8b
deadcam2020 <85250604+deadcam2020@users.noreply.github.com>
2021-10-09 15:39:59 -0500

    Delete index.php

--- index.php
+++
@@ -1,9 +0,0 @@
-<?php 
-$destinatario = "camilocontreras2019@gmail.com"; 
-$asunto = "Este mensaje es de prueba"; 
-$mensaje = "Hola, esta es una prueba de cómo envíar correos electrónicos con PHP";
-
-
-
-mail($destinatario,$asunto,$mensaje) 
-?>
