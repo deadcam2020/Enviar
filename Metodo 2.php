4915097fe8b106583de4864810d462ea71d81343
deadcam2020 <85250604+deadcam2020@users.noreply.github.com>
2021-10-09 15:42:51 -0500

    Update Metodo 2.php

--- Metodo 2.php
+++ Metodo 2.php
@@ -4,7 +4,9 @@ $asunto = "Este mensaje es de prueba";
 $mensaje = "Hola, esta es una prueba de cómo envíar correos electrónicos con PHP";
 
 $nombre = $_POST['nombre'];
+$telefono = $_POST['telefono'];
 
+$info = "Nombre: ".$nombre. "Telefono: ".$telefono;
 
-mail($destinatario,$asunto,$mensaje); 
+mail($destinatario,$asunto,$mensaje, $info); 
 ?>
